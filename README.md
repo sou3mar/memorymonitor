d496e30b636354292e9dda42c74ea9b2c919c2f0
Amirmohsen <sistemfa.ir@gmail.com>
2021-09-08 15:59:35 +0430

    Did some cleaning / Screenshot added.

--- README.md
+++ README.md
@@ -1,37 +1,39 @@
 ## Introduction
-
+![Screenshot](https://i.imgur.com/CUomBwB.jpg)
 This simple project will monitor the free/used memory in a 5s cycle.
-
 Packages used: [Chalk](https://www.npmjs.com/package/chalk "Chalk") & [OS-Monitor](https://www.npmjs.com/package/OS-Monitor "OS-Monitor")
 
 ## Install & Run!
+Firstly, install all of dependencies using this command:
 
-First, to install all of dependencies, use this command:
+```sh
+npm i
+```
 
-     npm install
-
-The latest version of each package will be downloaded, according to "**package.json**" file!
-
+The latest version of each package will be downloaded, according to "**package.json**" file.
 
 To install packages individually:
+```sh
+npm install --save os-monitor
+npm install --save chalk
+```
+     
+After installation, head to the directory in which your project has been stored. (Using Command Prompt / Terminal)
 
-     npm install --save os-monitor
-     npm install --save chalk
+```sh
+cd C:\path\memorymonitor
+```
      
-After installation, head to the directory in which your project has been stored! (Using Command Prompt / Terminal) You can easily use this command in Windows & it works on GNU/Linux distributions as Well!
+Then enter one of the following commands to run the program:
 
-     cd C:\example\
+```sh
+node index.js
+npm start
+```
      
-Then enter one of the followings:
+*npm start* is defined as a script in **package.json** file.
 
-     node index.js
-     or
-     npm start
-     
-"*npm start*" is defined as a script in **package.json** file!
-     
-The project will be running and would show the memory state every 5 seconds!
+The program will be running and would log the memory state every 5 seconds.
 
 ## Copyright
-
 This project is under MIT license. :)
--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-"use strict";
+'use strict';
 
-const chalk=require("chalk");
-const monitor=require("os-monitor");
-const log=console.log;
-const red=chalk.bold.white.bgRed;
-const green=chalk.bold.white.bgGreen;
-const blue=chalk.bold.white.bgBlue;
-var count=1;
+const chalk		=	require('chalk')
+const monitor	=	require('os-monitor')
+const log     	=	console.log
+const red     	=	chalk.bold.white.bgRed
+const green   	=	chalk.bold.white.bgGreen
+const blue    	=	chalk.bold.white.bgBlue
+let   count     =	1
 
-function formatBytes(a,b) {if(0==a)return"0 Bytes";var c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
+function formatBytes(a, b) {
+	// 1 Gigabyte = 1024 Megabytes
+	if(a === 0) return "0 Bytes"
+	let c = 1024,
+		d = b || 2,
+		e = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
+		f = Math.floor(Math.log(a) / Math.log(c));
+	return parseFloat((a / Math.pow(c, f)).toFixed(d)) + " " + e[f]
+}
 
 function now() {
-  var d=new Date(),h=(d.getHours()<10?'0':'')+d.getHours(),m=(d.getMinutes()<10?'0':'')+d.getMinutes(),s=(d.getSeconds()<10?'0':'')+d.getSeconds();return h+':'+m+':'+s;
+	let d = new Date(),
+  		h = (d.getHours() < 10 ? '0' : '') + d.getHours(),
+		m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes(),
+		s = (d.getSeconds() < 10 ? '0' : '') + d.getSeconds()
+	return h + ':' + m + ':' + s
 }
 
 monitor.start({
-    delay: 5000, // in ms
-    freemem: 1000000000,
+    delay: 5000, // delay in mili-seconds (currently 5 seconds)
+    freemem: 1000000000, // minimum memory warning threshold (currently 1 GBs)
     immediate: true,
     silent: false
-});
+})
 
-monitor.on('monitor',function(event) {
-    var totalmem=event.totalmem,freemem=event.freemem,using=formatBytes(totalmem-freemem,3);
-    if(count===1) log(green(" Total Memory:")+" "+formatBytes(totalmem,3));
-    log(blue(count)+red(" Free Memory:")+" "+formatBytes(freemem,4)+" / Using: "+using+" / "+now());
-    count++;
-});
+monitor.on('monitor', event => {
+    let totalmem =	event.totalmem,
+		freemem	 =	event.freemem,
+		using	 =	formatBytes((totalmem - freemem), 3)
+	
+    if(count === 1) log(green(" Total Memory:") + " " + formatBytes(totalmem, 3)); // Show total memory on start
+    log(blue(count) + red(" Free Memory:") + " " + formatBytes(freemem, 4) + " / Using: " + using + " / " + now());
 
-monitor.on('freemem', function(event) {
-  log(red(" Warning:")+" Memory is less than 1GBs!");
-});
+    ++count;
+})
+
+monitor.on('freemem', () => {
+	log(red(" • Warning:") + " Memory is less than 1 GBs!");
+})
